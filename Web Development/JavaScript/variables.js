db768a866a2010db1e6dd1d079526d6b0744dc35
khushi0704 <khushi.jain0704@gmail.com>
2022-10-08 18:48:52 +0530

    intro to js

---
+++ Web Development/JavaScript/variables.js
@@ -0,0 +1,11 @@
+function swap(){
+    var a=3;
+    var b=8;
+    var c;
+    console.log("before swap : " + a + " " + b);
+    c=a;
+    a=b;
+    b=c;
+    console.log("after swap : " + a + " " + b);
+}
+swap();
